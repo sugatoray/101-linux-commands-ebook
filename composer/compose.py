be35219b6a7bdb433399dd227ac7aa180fd8acbc
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-12 14:30:16 -0600

    added section for "Download" to composer - "composer" automates generation of nav in mkdocs.yml - refactored composer.py

--- composer/compose.py
+++ composer/compose.py
@@ -6,27 +6,35 @@ import json
 import re
 import yaml
 import argparse
+from typing import List, Tuple, Dict, Union, Any, Optional, Callable, NewType
 
 PAT = re.compile(".*(?P<command_id>\d{3})-(?:the-)?(?P<slug>.*)-command.md")
 PREFIX = '<strong>'
 SUFFIX = '</strong>'
 NAV_PATTERN = "<code>{command_id}:&nbsp;{slug}</code>"
 
+SlugmapType = NewType("SlugmapType", Dict[str, Dict[str, str]])
+CommandNavsType = NewType("CommandNavsType", List[Dict[str, str]])
+NavsDataType = NewType("NavsDataType", Dict[str, List[Dict[str, List[str, str]]]])
+
 # docs_dir = "../docs"
 # files = glob(f'{docs_dir}/ebook/en/content/*.md')
 # files.sort()
 #print(files)
 
-def generate_nav(docs_dir: str='../docs', dry_run: bool=False, create_slugmap: bool=True, ebook_pattern: str="ebook/en/content/*.md"):
+def get_navs_skeleton(
+        files: List[str], 
+        docs_dir: str='../docs', 
+        command_filename_endswith: str="command.md",
+        **kwargs
+    ) -> Tuple[SlugmapType, CommandNavsType]:
+    """Returns nav skeleton in the form of a list (`command_navs`) and a dict (`slugmap`)."""
 
-    files = glob(f'{docs_dir}/{ebook_pattern}')
-    files.sort()
     command_navs = []
     slugmap = dict()
-    #print(len(files))
 
     for file in files:
-        if file.endswith("command.md"):
+        if file.endswith(command_filename_endswith):
             #slug = PAT.findall(file)[0]
             match = PAT.match(file).groupdict()
             slug = match.get('slug')
@@ -46,32 +54,81 @@ def generate_nav(docs_dir: str='../docs', dry_run: bool=False, create_slugmap: b
 
             command_navs.append({NAV_PATTERN.format(slug=slug, command_id=command_id): path})
 
+    return slugmap, command_navs 
+
+def create_navs_data(command_navs: CommandNavsType, **kwargs) -> NavsDataType:
+
+    navs_data = {
+        "nav": [
+            {"<p><i class='fas fa-home'>&nbsp;</i> Home</p>": "index.md"},
+            {
+                "<p><i class='fas fa-terminal'>&nbsp;</i>Commands</p>": command_navs +
+                    [{"Wrap Up": "ebook/en/content/999-wrap-up.md"}],
+            },
+            {
+                "<p><i class='fas fa-download'>&nbsp;</i>Download</p>": [
+                    {"Download eBook": "download.md"},
+                ],
+            },
+            {
+                "<p><i class='fas fa-info-circle'>&nbsp;</i>About</p>": [
+                    {"Info": "about/index.md"},
+                    {"License": "about/license.md"},
+                ],
+            },
+        ],
+    }
+
+    return navs_data
+
+def generate_nav(
+        docs_dir: str='../docs', 
+        dry_run: bool=False, 
+        create_slugmap: bool=True, 
+        ebook_pattern: str="ebook/en/content/*.md",
+        command_filename_endswith: str="command.md",
+        slugmap_filename: str="slugmap.json",
+        command_navs_filename: str="command_navs.yml",
+        **kwargs
+    ):
+    """Generates nav structure from ebook content-files."""
+
+    # Acquire target file names
+    files = glob(f'{docs_dir}/{ebook_pattern}')
+    files.sort()
+    
+    #print(len(files))
+
+    # Generate nav skeleton for commands
+    # :: for each "*-command.md" file:
+    # - slugmap: a key-value mapping of slug used.
+    # - command_navs: a list of key-value mapping of navs.
+    slugmap, command_navs = get_navs_skeleton(
+        files, 
+        docs_dir=docs_dir, 
+        command_filename_endswith=command_filename_endswith,
+        **kwargs
+    )
+
+    # Write slugmap to a persistent file: 'slugmap.json'
     if create_slugmap:
-        with open(f'{docs_dir}/slugmap.json', 'w') as f:
+        with open(f'{docs_dir}/{slugmap_filename}', 'w') as f:
             f.write(json.dumps(slugmap, indent=2))
 
-    navs_data = {"nav": [
-        {"<p><i class='fas fa-home'>&nbsp;</i> Home</p>": "index.md"},
-        {
-            "<p><i class='fas fa-terminal'>&nbsp;</i>Commands</p>": command_navs +
-                [{"Wrap Up": "ebook/en/content/999-wrap-up.md"}],
-        },
-        {
-            "<p><i class='fas fa-info-circle'>&nbsp;</i>About</p>": [
-                {"Info": "about/index.md"},
-                {"License": "about/license.md"},
-            ],
-        },
-    ]}
+    # Generate navigation structure for mkdocs.yml
+    navs_data = create_navs_data(command_navs, **kwargs)
 
+    # Display/Write to file the navs-struture
     if dry_run:
+        # Display
         yaml.dump(navs_data, None,
-                  default_flow_style=False,
-                  default_style='"',
-                  indent=2,
-                  )
+            default_flow_style=False,
+            default_style='"',
+            indent=2,
+        )
     else:
-        with open(f'{docs_dir}/command_navs.yml', 'w') as f:
+        # Write to file: 'command_navs.yml'
+        with open(f'{docs_dir}/{command_navs_filename}', 'w') as f:
             yaml.dump(navs_data, f,
                 default_flow_style=False,
                 default_style='"',
